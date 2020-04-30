e8f31d437daa3b64906ef1e8b3db216b426c4bac
Sergey Grigoriev <sergey00475@gmail.com>
2020-04-30 11:13:13 +0400

    Добавьте файлы проекта.

---
+++ TreeClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeClass
+{
+    class Tree
+    {
+        public double price { get; set; }
+        public int age { get; set; }
+        public string place { get; set; }
+        public string name { get; set; }
+
+        public void GetTreeInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Место: {place}|");
+        }
+    }
+
+
+    class Tree1 : Tree
+    {
+        public string type { get; set; }
+
+        public virtual void GetTree1Info()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Место: {place}| |Цена саженца: {price}| |Тип: {type}");
+        }
+    }
+}
---
+++ TreeClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tree tree = new Tree();
+            tree.name = "Дуб";
+            tree.price = 45;
+            tree.age = 30;
+            tree.place = "Лесная зона";
+
+            tree.GetTreeInfo();
+
+            Tree1 tree1 = new Tree1();
+            tree1.type = "Берёзовые";
+            tree1.name = "Берёза";
+            tree1.place = "Северное полушарие";
+            tree1.price = 100;
+            tree1.age = 20;
+
+
+            tree1.GetTree1Info();
+
+            Console.ReadLine();
+        }
+    }
+}
